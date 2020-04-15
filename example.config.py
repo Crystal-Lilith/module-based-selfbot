7a2f93b3c09c20a22c0b152a54ed09d2d3dedd0f
Crystal|Lilith <crystalneon786@gmail.com>
2020-04-15 07:59:58 +0100

    I fixed some of my grammar

--- example.config.py
+++ example.config.py
@@ -1,9 +1,9 @@
 token = "Put your token here"
-prefix = "/" # Replace with your preffered prefix
+prefix = "/" # Replace with your preferred prefix
 enabled_by_default = False # Recommened to have this as False, if you wish to enable it in a certain channel do /enablw_channel
-splitter = "][" # This is how arguments are made, so for example `/msg 10][Hi` has 2 arguments, which is split by '][' by default
+splitter = "][" # This is how arguments are split, so for example `/msg 10][Hi` has 2 arguments, which is split by '][' by default
 
-# The code below checks if the code is valid
+# The code below checks if the config is valid
 for i in (token, prefix, splitter):
     if not isinstance(i, str):
         raise SystemExit("token, prefix or splitter is not a string, please make sure they are all strings")
